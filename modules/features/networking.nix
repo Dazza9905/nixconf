857d014454704e039cb4c4e81115126b4138379f
Dazza9905 <darendrahos@fastmail.com>
2026-06-11 00:39:31 +0200

    .

--- modules/features/networking.nix
+++ modules/features/networking.nix
@@ -1,18 +1,24 @@
 { self, inputs, ... }: {
-  flake.nixosModules.networking = { pkgs, lib, ... }: {
+  flake.nixosModules.networking = { pkgs, lib, username, ... }: {
         services.netbird.clients.wt0 = {
-
-        # Port used to listen to wireguard connections
         port = 51821;
-
-        # Set this to true if you want the GUI client
         ui.enable = false;
-
-        # This opens ports required for direct connection without a relay
         openFirewall = true;
-
-        # This opens necessary firewall ports in the Netbird client's network interface
         openInternalFirewall = true;
     };
+
+    users.users.${username}.extraGroups = [ "netbird-wt0" ];
+
+    #map origo command
+    systemd.tmpfiles.rules = [
+      "d /var/run/netbird 0755 root root -"
+      "L+ /var/run/netbird.sock - - - - /var/run/netbird-wt0/sock"
+    ];
+
+    environment.systemPackages = [
+      (pkgs.writeShellScriptBin "netbird" ''
+        exec /run/current-system/sw/bin/netbird-wt0 "$@"
+      '')
+    ];
   };
 }
