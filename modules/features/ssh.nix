f9a99654cc39e5792563e43956e3072d1d2bc547
Dazza9905 <darendrahos@fastmail.com>
2026-07-11 11:47:31 +0200

    gen 85 (2026-07-11 11:47:28)

--- modules/features/ssh.nix
+++ modules/features/ssh.nix
@@ -14,9 +14,9 @@
       openFirewall = true;
       settings = {
         PasswordAuthentication = true;
-        KbdInteractiveAuthentication = false;
+        KbdInteractiveAuthentication = true;
         PermitRootLogin = "no";
-        AllowUsers = ["myUser"];
+        AllowUsers = ["dazza"];
         MaxAuthTries = 3;
         PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
       };
