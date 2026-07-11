55a16be873f9aad549f5c70c55276643665cd51f
Dazza9905 <darendrahos@fastmail.com>
2026-07-11 10:41:21 +0200

    gen 82 (2026-07-11 10:41:17)

--- modules/hosts/dazzapc/configuration.nix
+++ modules/hosts/dazzapc/configuration.nix
@@ -15,6 +15,7 @@
       self.nixosModules.devices
       self.nixosModules.desktop
 
+      self.nixosModules.networking
       self.nixosModules.starcitizen
       self.nixosModules."programs-3d"
       self.nixosModules.sunshine
